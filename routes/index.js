92738887a5600b41501907d56b3c35515b841f3b
Lyndsay Futrell <lyndsayfutrell@gmail.com>
2022-11-14 15:36:17 -0500

    completed search feauture

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const Book = require('../models').Book;
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
+const limit = 10;
 
 function asyncHandler(cb){
   return async(req, res, next) => {
@@ -21,10 +22,14 @@ router.get('/', (req, res, next) => {
   res.redirect("/books/page1")
 });
 
+/* GET home page. */
+router.get('/books/', (req, res, next) => {
+  res.redirect("/books/page1")
+});
+
 /* GET books */
 router.get('/books/page:number', asyncHandler(async (req, res, next) => {
   let currentPage = req.params.number;
-  let limit = 10;
   let offset = 0 + (currentPage - 1) * limit;
   const pageTotal = Math.ceil(((await Book.findAll({order:[["title"]]})).length) / 10);
   const books = await Book.findAll({order:[["title"]], limit: limit, offset: offset,});
@@ -104,11 +109,68 @@ router.post('/books/:id/delete', asyncHandler(async (req ,res) => {
   const book = await Book.findByPk(req.params.id);
   if(book) {
     await book.destroy();
-    res.redirect("/books/");
+    res.redirect("/books/page1");
   } else {
     next(createError(404));
   }
 }));
 
+
+
+
+/* Search */
+
+router.get('/books/page:number/:search', asyncHandler(async (req ,res) => {
+  const value = req.params.search;
+  let currentPage = req.params.number;
+  const total = await Book.findAll({where: {
+    [Op.or]: [
+      {author: {
+        [Op.like]: `%${value}%`
+        }
+      },
+      {genre: {
+        [Op.like]: `%${value}%`
+        }
+      },
+      {title: {
+        [Op.like]: `%${value}%`
+        }
+      },
+    ]
+    }});
+    let pageTotal = Math.ceil((total.length) /10);
+    let pages = [];
+    let offset = 0 + (currentPage - 1) * limit;
+    console.log(pageTotal);
+    for (i=1; i <=pageTotal; i++ ) {
+      pages += i;
+    }
+    const books = await Book.findAll({where: {
+    [Op.or]: [
+      {author: {
+        [Op.like]: `%${value}%`
+        }
+      },
+      {genre: {
+        [Op.like]: `%${value}%`
+        }
+      },
+      {title: {
+        [Op.like]: `%${value}%`
+        }
+      },
+    ]
+    }, order:[["title"]], limit: limit, offset:offset });
+    console.log(pages);
+    res.render('index', {books, pages, value} );
+  
+}));
+
+
+router.post('/books/page:number/:search', asyncHandler(async (req ,res) => {
+  search = req.body.search;
+  res.redirect("/books/page"+ 1 + "/" + search);
+}));
 
 module.exports = router;
